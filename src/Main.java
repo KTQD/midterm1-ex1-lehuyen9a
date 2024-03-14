5a980901c07b8d23e882814b31aa1a3b47565e97
lehuyen9a <146607449+lehuyen9a@users.noreply.github.com>
2024-03-14 07:48:15 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,9 @@
 public class Main {
     public static void main(String[] args) {
 
-        MP3 mp3;
+        MP3 mp3 = new MP3("MP3", "bai hat", "MP01", 495, 120);
+        Book book = new Book("sach toan", "toan 12", "01", 125, "ngoc", 400, "baitap");  
         mp3.showInfo();
-
-        Book book;
         book.showInfo();
-
     }
 }
